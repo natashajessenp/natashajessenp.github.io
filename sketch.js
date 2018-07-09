0db2800cbf5b96998f1e8ee6fc80c65131231d28
Natasha Jessen-Petersen <natashajessenp@gmail.com>
2018-07-09 15:50:06 +0200

    Second Circle

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 function setup() {
-  // put setup code here
+  createCanvas(600,400)
 }
 
 function draw() {
-  // put drawing code here
-  ellipse(40, 40, 40, 40);
-  
+  background('lightgreen');
+//this circle will follow my mouse
+  fill("gray");
+  ellipse(mouseX, mouseY, 40, 40);
+
+  fill ("blue");
+  textsize(32);
+  text("happy copenhagen", 50, 100);
+
 }
